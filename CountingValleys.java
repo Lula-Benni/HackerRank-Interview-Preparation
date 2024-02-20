0979bb8e279eb5a786af88027aea215c5aa34ac4
lulamile99 <lulabenni45@gmail.com>
2024-02-20 21:03:38 +0200

    Completed counting valleys problem

---
+++ CountingValleys.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class CountingValleys {
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int valleys = 0;
+        int stepsUp = 0;
+
+        int stepsDown = 0;
+        for(int i=0;i<=path.length()-1;i++){
+            String stepType = path.substring(i,i+1);
+            if(stepType.equals("U")){
+                stepsUp++;
+            }
+            else if(stepType.equals("D")){
+                stepsDown++;
+            }if (stepsUp==stepsDown&&stepType.equals("U")) {
+                valleys++;
+                System.out.println(valleys+" "+i);
+            }
+        }
+        return valleys;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CountingValleys.countingValleys(8, "DDUUUUDD"));
+    }
+}
